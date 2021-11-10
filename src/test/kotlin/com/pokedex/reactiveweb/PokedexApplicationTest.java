c1fb6435a663101e5c5f89590589ad07295c04fa
marcelorodriguesdev <msilva8@uolinc.com>
2021-11-09 22:00:08 -0300

    adiciona arquivos

---
+++ src/test/kotlin/com/pokedex/reactiveweb/PokedexApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pokedex.reactiveweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
